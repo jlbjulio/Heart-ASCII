705d73afd8c8ee60f0a4cf7bde3de10d7592564c
Julio Lara <juliojose789@gmail.com>
2025-07-30 19:16:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,21 @@ const heart = `
 `;
 
 const message = `
-  TTTTT  EEEEE      A    M   M   OOO       M   M   U   U   CCCC   H   H   OOO  
-    T    E         A A   MM MM  O   O      MM MM   U   U  C       H   H  O   O 
-    T    EEEE     AAAAA  M M M  O   O      M M M   U   U  C       HHHHH  O   O 
-    T    E       A     A M   M  O   O      M   M   U   U  C       H   H  O   O 
-    T    EEEEE  A       A M   M   OOO       M   M    UUU    CCCC   H   H   OOO  
+  TTTTT  EEEEE      A    M   M   OOO       M   M   U   U   CCCC   H   H   OOO       M   M  IIIII      A    M   M   OOO   RRRR  
+    T    E         A A   MM MM  O   O      MM MM   U   U  C       H   H  O   O     MM MM    I        A A   MM MM  O   O  R   R 
+    T    EEEE     AAAAA  M M M  O   O      M M M   U   U  C       HHHHH  O   O     M M M    I       AAAAA  M M M  O   O  RRRR  
+    T    E       A     A M   M  O   O      M   M   U   U  C       H   H  O   O     M   M    I      A     A M   M  O   O  R   R 
+    T    EEEEE  A       A M   M   OOO       M   M    UUU    CCCC   H   H   OOO      M   M  IIIII   A       A M   M   OOO   R   R 
+`;
+
+
+const dinosaur = `
+               __
+              / _)
+     _.----._/ /
+    /         /
+ __/ (  | (  |
+/__.-'|_|--|_|
 `;
 
 function randomizeText(text) {
@@ -36,5 +46,5 @@ const container = document.getElementById('ascii-heart');
 // Animar solo el corazón
 setInterval(() => {
   const scrambledHeart = randomizeText(heart);
-  container.textContent = scrambledHeart + '\n\n' + message;
+  container.textContent = scrambledHeart + '\n\n' + message + '\n\n' + dinosaur;
 }, 100);
