3644ef7a2cf155fe2ce0a3d19a0ba49ae469cf8b
Julio Lara <juliojose789@gmail.com>
2025-07-31 15:51:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ const heart = `
 `;
 
 const message = `
-  TTTTT  EEEEE      A    M   M   OOO       M   M   U   U   CCCC   H   H   OOO       M   M  IIIII      A    M   M   OOO   RRRR  
-    T    E         A A   MM MM  O   O      MM MM   U   U  C       H   H  O   O     MM MM    I        A A   MM MM  O   O  R   R 
-    T    EEEE     AAAAA  M M M  O   O      M M M   U   U  C       HHHHH  O   O     M M M    I       AAAAA  M M M  O   O  RRRR  
-    T    E       A     A M   M  O   O      M   M   U   U  C       H   H  O   O     M   M    I      A     A M   M  O   O  R   R 
+  TTTTT  EEEEE      A     M   M   OOO       M   M   U   U   CCCC   H   H   OOO      M   M  IIIII       A     M   M   OOO   RRRR  
+    T    E         A A    MM MM  O   O      MM MM   U   U  C       H   H  O   O     MM MM    I        A A    MM MM  O   O  R   R 
+    T    EEEE     AAAAA   M M M  O   O      M M M   U   U  C       HHHHH  O   O     M M M    I       AAAAA   M M M  O   O  RRRR  
+    T    E       A     A  M   M  O   O      M   M   U   U  C       H   H  O   O     M   M    I      A     A  M   M  O   O  R   R 
     T    EEEEE  A       A M   M   OOO       M   M    UUU    CCCC   H   H   OOO      M   M  IIIII   A       A M   M   OOO   R   R 
 `;
 
